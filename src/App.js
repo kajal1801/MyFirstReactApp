3dac60b1c62c67649fe2c06b763678f778e48d35
kajal1801 <anshikajais2001@gmail.com>
2023-04-16 21:29:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+function MyButton() {
+  return (
+    <>
+      <a className='btn' href='https://react.dev' rel='noreferrer' target='_blank'>Learn More</a>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my first <span id='react'>React</span> app</h1>
+      <MyButton />
     </div>
   );
 }
